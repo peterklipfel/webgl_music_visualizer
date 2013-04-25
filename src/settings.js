05a3dbc792d04211a3a5dd69048b7b2d272d0385
Peter Klipfel <peter@klipfel.me>
2013-04-24 21:33:58 -0600

    input knobified

---
+++ src/settings.js
@@ -0,0 +1,10 @@
+$(function() {
+    $(".dial").knob({
+    	'width': 60,
+    	'height': 60,
+    	// 'fgColor': "#000",
+    	'fgColor':"#FFEC03",
+		'bgColor':'#000',
+		'displayPrevious': 'true'
+    });
+});
